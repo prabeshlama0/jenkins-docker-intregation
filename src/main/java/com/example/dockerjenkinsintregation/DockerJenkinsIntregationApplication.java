c22acc34b6a8ae19affd06dd6943e8f36d0a23a0
prabeshlama <prabeshlama0@gmail.com>
2021-09-16 20:42:49 -0500

    firstCommit

---
+++ src/main/java/com/example/dockerjenkinsintregation/DockerJenkinsIntregationApplication.java
@@ -0,0 +1,14 @@
+package com.example.dockerjenkinsintregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntregationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntregationApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
